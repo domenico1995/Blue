52f4cf716bb0108fce80eb67b125ab5170a67155
progect-cloud <progect-cloud@outlook.it>
2023-05-18 13:07:41 +0200

    prima aggiunta

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String testo;
+
+        //problema numero uno estrapolare le informazioni e i comandi
+
+        System.out.print("inserire testo di prova: ");
+
+        testo = sc.nextLine();
+
+        if(testo.contains("printf")){
+
+        } else if (testo.contains("int")) {
+
+        }
+        System.out.println("Hello world!");
+
+    }
+}
